5e4658b1b98f038b2721c8a2492d384c4859ac58
Abhay Mudgal <52732872+mudgalabhay@users.noreply.github.com>
2021-11-20 19:36:34 +0530

    Update settings.py

--- DogsVsCats/settings.py
+++ DogsVsCats/settings.py
@@ -120,9 +120,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
+
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = "/static/"
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "/static/uploaded_images/")
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
